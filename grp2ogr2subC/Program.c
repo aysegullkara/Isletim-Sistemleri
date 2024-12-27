6351557a1b95915c9606fda5b9088454d4e140ba
melked <ce.melikedemirtas@gmail.com>
2024-12-27 22:24:20 +0300

    Increment eklendi.

--- grp2ogr2subC/Program.c
+++ grp2ogr2subC/Program.c
@@ -237,7 +237,141 @@ int execute_command(char** args, char* girisDosyasi, char* cikisDosyasi, int ark
     return 0;
 }
 
+// Pipe ile bağlanmış komutları çalıştırır
+// @param pipeCmd: Pipe ile ayrılmış komutlar dizisi
+// @param pipeSayac: Pipe sayısı
+// @param globalGirisDosyasi: Tüm pipeline için input redirection
+// @param globalCikisDosyasi: Tüm pipeline için output redirection
+int execute_pipe_commands(char** pipeCmd, int pipeSayac, char* globalGirisDosyasi, char* globalCikisDosyasi) {
+    int cmdNumara = pipeSayac + 1;
+    int i;
+    // Pipe'lar için dosya tanımlayıcıları oluştur
+    int pipes[2 * pipeSayac];
+    for (i = 0; i < pipeSayac; i++) {
+        if (pipe(pipes + i * 2) < 0) {
+            perror("pipe");
+            return -1;
+        }
+    }
 
+    // Her komut için bir süreç oluştur
+    for (i = 0; i < cmdNumara; i++) {
+        char* args[MAX_ARGS];
+        char* girisDosyasi = NULL;
+        char* cikisDosyasi = NULL;
+        int arkaplan = 0;
+        char** dummy_pipe = NULL;
+        int dummy_count = 0;
+
+        // Komutu parse et
+        parse_command(pipeCmd[i], args, &girisDosyasi, &cikisDosyasi, &arkaplan, &dummy_pipe, &dummy_count);
+
+        pid_t pid = fork();
+        if (pid == 0) { // Child process
+            // İlk komut için global input redirection
+            if (i == 0 && globalGirisDosyasi) {
+                int dosya = open(globalGirisDosyasi, O_RDONLY);
+                if (dosya < 0) {
+                    fprintf(stderr, "%s giriş dosyası bulunamadı.\n", globalGirisDosyasi);
+                    exit(1);
+                }
+                dup2(dosya, STDIN_FILENO);
+                close(dosya);
+            }
+
+            // Son komut için global output redirection
+            if (i == cmdNumara - 1 && globalCikisDosyasi) {
+                int dosya = open(globalCikisDosyasi, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+                if (dosya < 0) {
+                    perror("output file");
+                    exit(1);
+                }
+                dup2(dosya, STDOUT_FILENO);
+                close(dosya);
+            }
+
+            // Pipe bağlantılarını ayarla
+            if (i > 0) {
+                dup2(pipes[(i - 1) * 2], STDIN_FILENO);
+            }
+            if (i < cmdNumara - 1) {
+                dup2(pipes[i * 2 + 1], STDOUT_FILENO);
+            }
+
+            // Kullanılmayan pipe'ları kapat
+            for (int j = 0; j < 2 * pipeSayac; j++) {
+                close(pipes[j]);
+            }
+
+            // Özel "increment" komutu kontrolü
+            if (strcmp(args[0], "increment") == 0) {
+                execute_increment(NULL);
+                exit(0);
+            }
+
+            // Komutu çalıştır
+            execvp(args[0], args);
+            perror("exec");
+            fprintf(stderr, "Komut bulunamadi: %s\n", args[0]);
+            exit(1);
+        }
+        else if (pid < 0) { // Fork hatası
+            perror("fork");
+            return -1;
+        }
+    }
+
+    // Parent process'te tüm pipe'ları kapat
+    for (int j = 0; j < 2 * pipeSayac; j++) {
+        close(pipes[j]);
+    }
+
+    // Tüm child process'lerin tamamlanmasını bekle
+    for (int k = 0; k < cmdNumara; k++) {
+        int durum;
+        wait(&durum);
+    }
+
+    return 0;
+}
+
+// Noktalı virgül ile ayrılmış sıralı komutları çalıştırır
+// @param line: Komut satırı
+void execute_sequential_commands(char* line) {
+    char* kayitPtr;
+    // Noktalı virgül ile ayrılmış komutları ayır
+    char* komut = strtok_r(line, ";", &kayitPtr);
+    while (komut != NULL) {
+        char args[MAX_ARGS][MAX_CMD_LEN];
+        char* girisDosyasi = NULL;
+        char* cikisDosyasi = NULL;
+        int arkaplan = 0;
+        int pipeSayac = 0;
+        char** pipeCmd = NULL;
+
+        // Komutu parse et
+        parse_command(komut, (char**)args, &girisDosyasi, &cikisDosyasi, &arkaplan, &pipeCmd, &pipeSayac);
+
+        // Pipe varsa pipe komutlarını çalıştır
+        if (pipeSayac > 0 && pipeCmd != NULL) {
+            execute_pipe_commands(pipeCmd, pipeSayac, girisDosyasi, cikisDosyasi);
+            // Belleği temizle
+            for (int i = 0; i < pipeSayac + 1; i++) {
+                free(pipeCmd[i]);
+            }
+            free(pipeCmd);
+        }
+        // Tek komut varsa normal çalıştır
+        else {
+            if (args[0] != NULL) {
+                execute_command((char**)args, girisDosyasi, cikisDosyasi, arkaplan);
+            }
+        }
+
+        // Sonraki komutu al
+        komut = strtok_r(NULL, ";", &kayitPtr);
+    }
+}
 
 // Ana program döngüsü
 int main() {
